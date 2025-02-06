50446c6eefd82258c023d4e869244ec2f5f23802
Mohanish <vmohanish2000@hotmail.com>
2025-02-06 18:13:28 +0530

    Added necessary comments for the generateDonationReceipt method.

--- FunctionalInterfaces/src/SupplierFunctionalInterface/DonationReceiptGenerateExample.java
+++ FunctionalInterfaces/src/SupplierFunctionalInterface/DonationReceiptGenerateExample.java
@@ -10,9 +10,10 @@ public class DonationReceiptGenerateExample {
         /* Generate Random Number */
         SecureRandom secureRandom = new SecureRandom();
         long uniqueNumber = secureRandom.nextLong();
-
+        // Implementation for Supplier.
 //        Supplier<String> uniqueReceipt = () -> "DON-" + LocalDateTime.now() + "-" + UUID.randomUUID().toString().replace("-", "").toUpperCase().substring(0,3);
         Supplier<String> uniqueReceipt = () -> "DON-" + LocalDateTime.now() + "-" + String.valueOf(uniqueNumber).replace("-","");
+        // Calling the abstract method of the Supplier Functional Interface.
         return  uniqueReceipt.get();
     }
 
