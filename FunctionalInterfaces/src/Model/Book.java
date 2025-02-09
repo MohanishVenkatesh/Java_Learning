3c5a31c0815a1c9918098bc23c1552345121ca33
Mohanish <vmohanish2000@hotmail.com>
2025-02-09 19:06:25 +0530

    Added lombok.jar and refactored the code according.

--- FunctionalInterfaces/src/Model/Book.java
+++ FunctionalInterfaces/src/Model/Book.java
@@ -1,7 +1,13 @@
 package Model;
 
 import Constants.Genre;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Book {
     private String title;
     private String author;
@@ -11,82 +17,4 @@ public class Book {
     private int year;
     private Genre genre;
     private Donor donor;
-
-    public Book() {
-    }
-
-    public Book(String title, String author, String publisher, String isbn, int pages, int year, Genre genre, Donor donor) {
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.isbn = isbn;
-        this.pages = pages;
-        this.year = year;
-        this.genre = genre;
-        this.donor = donor;
-    }
-
-    public Donor getDonor() {
-        return donor;
-    }
-
-    public void setDonor(Donor donor) {
-        this.donor = donor;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
 }
